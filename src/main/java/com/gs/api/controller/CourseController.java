32d2a9809bc1b662a04b02b7dbb0dd24cb541306
Eric Taylor <taylor_eric@yahoo.com>
2015-07-14 14:23:52 -0400

    Removed commented out lines from imports in Controller

--- src/main/java/com/gs/api/controller/CourseController.java
+++ src/main/java/com/gs/api/controller/CourseController.java
@@ -3,8 +3,6 @@ package com.gs.api.controller;
 import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
-//import org.codehaus.jackson.JsonGenerationException;
-//import org.codehaus.jackson.map.JsonMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
