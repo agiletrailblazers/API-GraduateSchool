d458e7163e7b05673539df7c8249fef96a95387e
Eric Taylor <taylor_eric@yahoo.com>
2015-07-23 17:25:13 -0400

    Add unit test for CourseSearch; Refactor RestTemplate for mocking

--- src/main/java/com/gs/api/service/CourseSearchServiceImpl.java
+++ src/main/java/com/gs/api/service/CourseSearchServiceImpl.java
@@ -3,11 +3,13 @@ package com.gs.api.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestOperations;
 
 import com.gs.api.domain.Course;
 import com.gs.api.domain.CourseSearchResponse;
@@ -21,6 +23,9 @@ public class CourseSearchServiceImpl implements CourseSearchService {
     
     @Value("${course.search.solr.endpoint}")
     private String courseSearchSolrEndpoint;
+
+    @Autowired(required=true)
+    private RestOperations restTemplate;
     
     /**
      * Perform a search for courses
@@ -31,29 +36,36 @@ public class CourseSearchServiceImpl implements CourseSearchService {
     public CourseSearchResponse searchCourses(String search) {
 
         boolean exactMatch = false;
+        int numFound = 0;
         
         //perform search
-        final RestTemplate restTemplate = new RestTemplate();
         final CourseSearchContainer container = restTemplate.getForObject(
                 courseSearchSolrEndpoint.replace("~", search), 
                 CourseSearchContainer.class);
-        logger.info("Found " + container.getResponse().getDocs().size() + " matches for search " + search);
-        
+
+        //log results
+        if (CollectionUtils.isNotEmpty(container.getResponse().getDocs())) {
+            numFound = container.getResponse().getDocs().size();
+        }
+        logger.info("Found " + numFound + " matches for search " + search);
+
         // loop through responses
         final CourseSearchResponse response = new CourseSearchResponse();
         List<Course> courses = new ArrayList<Course>();
-        for (CourseSearchDoc doc : container.getResponse().getDocs()) {
-            Course newCourse = new Course(doc.getCourse_id(), doc.getCourse_name(),
-                            doc.getCourse_description());
-            courses.add(newCourse);
-            //if the course id returned is exactly the same as the search string, this is 
-            //  almost certainly an exact match
-            if (doc.getCourse_id().equals(search)) {
-                exactMatch = true;
+        if (CollectionUtils.isNotEmpty(container.getResponse().getDocs())) {
+            for (CourseSearchDoc doc : container.getResponse().getDocs()) {
+                Course newCourse = new Course(doc.getCourse_id(), doc.getCourse_name(),
+                                doc.getCourse_description());
+                courses.add(newCourse);
+                //if the course id returned is exactly the same as the search string, this is 
+                //  almost certainly an exact match
+                if (doc.getCourse_id().equals(search)) {
+                    exactMatch = true;
+                }
             }
+            response.setCourses(courses.toArray(new Course[courses.size()]));
         }
-        response.setNumFound(container.getResponse().getDocs().size());
-        response.setCourses(courses.toArray(new Course[courses.size()]));
+        response.setNumFound(numFound);
         response.setExactMatch(exactMatch);
         return response;
     }
