eb55238023c6fa362d1e8a6d7293d54c7ac85843
Eric Taylor <taylor_eric@yahoo.com>
2015-07-24 18:02:30 -0400

    Refactor course detail pojos and updated/fixed some unit tests

---
+++ src/main/java/com/gs/api/domain/CourseSchedule.java
@@ -0,0 +1,11 @@
+package com.gs.api.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.ALWAYS)
+public class CourseSchedule {
+
+    //TODO: add in a future story
+    
+}
