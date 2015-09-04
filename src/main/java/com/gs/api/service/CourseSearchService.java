7f65827272331653d9325305ac9ab84422cb5644
Eric Taylor <taylor_eric@yahoo.com>
2015-09-04 10:50:58 -0400

    Split solr endpoint into environment specific part and to the query part which does not change per environment

--- src/main/java/com/gs/api/service/CourseSearchService.java
+++ src/main/java/com/gs/api/service/CourseSearchService.java
@@ -7,7 +7,7 @@ public interface CourseSearchService {
 
     public CourseSearchResponse searchCourses(String search, int start, int numRequested) throws NotFoundException;
     
-    public String buildSearchString(String endpoint, String search, int start, int numRequested);
+    public String buildSearchString(String search, int start, int numRequested);
     
     public String stripAndEncode(String search);
 
