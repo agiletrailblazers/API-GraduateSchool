1623e5437afd272545420e34c3ff79c4a8d6ed8c
Rahul Eturu <rahul7155@gmail.com>
2015-09-08 10:40:26 -0400

    TK-01875 Added the organized the imports statement

--- src/main/java/com/gs/api/controller/CourseController.java
+++ src/main/java/com/gs/api/controller/CourseController.java
@@ -15,8 +15,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
 
 import com.gs.api.domain.Course;
 import com.gs.api.domain.CourseSearchResponse;
