e83baea01b6544d350e1e063be2aced62efad6bd
Eric Taylor <taylor_eric@yahoo.com>
2015-10-19 14:19:12 -0400

    Updated Category domein object with addiitonal property and constructor

--- src/main/java/com/gs/api/domain/CourseCategory.java
+++ src/main/java/com/gs/api/domain/CourseCategory.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseCategory {
 
+    private String category;
     private CourseSubject[] CourseSubject;
 
+    //default constructor
+    public CourseCategory() {
+    }
+    
+    //convenience constructor
     public CourseCategory(CourseSubject[] courseSubject) {
         this.CourseSubject = courseSubject;
     }
@@ -17,5 +23,14 @@ public class CourseCategory {
     public void setCourseSubject(CourseSubject[] courseSubject) {
         CourseSubject = courseSubject;
     }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
 }
 
