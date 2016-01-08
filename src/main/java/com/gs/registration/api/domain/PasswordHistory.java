4807db731c28e6aa74902d57429da11c5aadf5fe
Ashley Hope <ahope@agiletrailblazers.com>
2016-01-08 15:56:26 -0500

    Implement packages and user data acces stubs

---
+++ src/main/java/com/gs/registration/api/domain/PasswordHistory.java
@@ -0,0 +1,9 @@
+package com.gs.registration.api.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class PasswordHistory {
+    /*  (user_id, password, ctr, created_on) */
+}
