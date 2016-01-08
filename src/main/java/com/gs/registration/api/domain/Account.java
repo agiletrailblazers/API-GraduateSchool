05372ee8116f352e86260d2382e36b89e7fc4c0b
Ashley Hope <ahope@agiletrailblazers.com>
2016-01-08 15:56:26 -0500

    Implement packages and user data acces stubs

---
+++ src/main/java/com/gs/registration/api/domain/Account.java
@@ -0,0 +1,8 @@
+package com.gs.registration.api.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class Account {
+    /* (xid, xcurrency_id, xsplit, xxlgen_account_no, xnewts */
+}
---
+++ src/main/java/com/gs/registration/api/domain/Address.java
@@ -0,0 +1,9 @@
+package com.gs.registration.api.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class Address {
+    /* (id, time_stamp, owner_id, addr1, addr2, addr3, city, state, zip, country, tag,
+    ci_city,ci_state,ci_country)*/
+}
